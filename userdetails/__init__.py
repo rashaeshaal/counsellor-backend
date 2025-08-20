f104c47daa1b956d9c58ce6d446e4d70198fdc68
rashaeshaal <rashaeshaal@gmail.com>
2025-08-20 13:46:52 +0530

    Fix requirements.txt encoding to UTF-8

--- userdetails/__init__.py
+++ userdetails/__init__.py
@@ -1,14 +1,24 @@
-import firebase_admin
-from firebase_admin import credentials
-from dotenv import load_dotenv
-import os
+# import firebase_admin
+# from firebase_admin import credentials
+# from dotenv import load_dotenv
+# import os
 
 # Load environment variables
-load_dotenv()
+# load_dotenv()
 
-# Initialize Firebase Admin SDK
-cred_path = os.getenv('FIREBASE_CREDENTIALS')
-if not os.path.exists(cred_path):
-    raise FileNotFoundError(f"Firebase credentials file not found at: {cred_path}")
-cred = credentials.Certificate(cred_path)
+# # Initialize Firebase Admin SDK
+# cred_path = os.getenv('FIREBASE_CREDENTIALS')
+# if not os.path.exists(cred_path):
+#     raise FileNotFoundError(f"Firebase credentials file not found at: {cred_path}")
+# cred = credentials.Certificate(cred_path)
+# firebase_admin.initialize_app(cred)
+
+import firebase_admin
+from firebase_admin import credentials
+import os
+import json
+
+cred_json = os.getenv('FIREBASE_CREDENTIALS')
+cred_dict = json.loads(cred_json)  # convert string to dict
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
