b6faa38debe1bceb692b364acafaa9858239e6aa
rashaeshaal <rashaeshaal@gmail.com>
2025-08-20 11:10:29 +0530

    added

--- counsellor_backend/settings.py
+++ counsellor_backend/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bhnzdvyujn1&qfi=u(@%t!6e_2-#12y&_y*ay)m1lu$0azak5*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -92,11 +92,11 @@ WSGI_APPLICATION = 'counsellor_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'counsellor',
-        'USER': 'postgres',
-        'PASSWORD': 'rasha@2048',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 REST_FRAMEWORK = {
