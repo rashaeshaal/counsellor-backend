feafda2e3db77444f853250f5df8eb5dca485125
rashaeshaal <rashaeshaal@gmail.com>
2025-09-01 17:17:15 +0530

    fix the firbase

--- userdetails/__init__.py
+++ userdetails/__init__.py
@@ -6,19 +6,12 @@ import os
 # Load environment variables
 load_dotenv()
 
+import json
+
 # Initialize Firebase Admin SDK
-cred_path = os.getenv('FIREBASE_CRED_PATH')
-if not os.path.exists(cred_path):
-    raise FileNotFoundError(f"Firebase credentials file not found at: {cred_path}")
-cred = credentials.Certificate(cred_path)
+cred_json = os.getenv('FIREBASE_CREDENTIALS')
+if not cred_json:
+    raise ValueError("FIREBASE_CREDENTIALS environment variable not set.")
+cred_dict = json.loads(cred_json)  # convert string to dict
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
-
-# import firebase_admin
-# from firebase_admin import credentials
-# import os
-# import json
-
-# cred_json = os.getenv('FIREBASE_CREDENTIALS')
-# cred_dict = json.loads(cred_json)  # convert string to dict
-# cred = credentials.Certificate(cred_dict)
-# firebase_admin.initialize_app(cred)
