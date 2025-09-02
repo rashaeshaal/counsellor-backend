a1a7c4fa03e246e8d1a3f45e2dc021b2964f13d4
rashaeshaal <rashaeshaal@gmail.com>
2025-09-02 10:37:43 +0530

    added import counserllor payment details in counsellor backend

--- counsellorapp/urls.py
+++ counsellorapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import UpcomingSessionsView, RecentActivityView, CounsellorProfileView, CounsellorPaymentSettingsView
+
+from .views import UpcomingSessionsView, RecentActivityView, CounsellorProfileView, CounsellorPaymentSettingsView,CounsellorPaymentDetailView
+
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
