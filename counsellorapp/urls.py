1424e304422e7f26db9fc2f67b58f81ad4b7cdfe
rashaeshaal <rashaeshaal@gmail.com>
2025-09-03 09:54:21 +0530

    fixed 500 eror in url

--- counsellorapp/urls.py
+++ counsellorapp/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('upcoming-sessions/', UpcomingSessionsView.as_view(), name='upcoming-sessions'),
     path('recent-activity/', RecentActivityView.as_view(), name='recent-activity'),
-    path('profile/', CounsellorProfileView.as_view(), name='counsellor-profile'),
+    path('counsellor-profile/', CounsellorProfileView.as_view(), name='counsellor-profile'),
     path('payment-settings/<int:user_id>/', CounsellorPaymentDetailView.as_view(), name='counsellor-payment-settings'),
     path('payment-settings/', CounsellorPaymentSettingsView.as_view(), name='payment-settings'),
     path('status/', CounsellorStatusView.as_view(), name='counsellor-status'),
